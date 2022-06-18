6ee6ac2111f320d58ff6179165e2664ef004fb43
username <bibekanandasingh0@gmail.com>
2022-06-19 01:16:35 +0530

    sass

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "../src/SASS/main.scss";
+import Begin from "./Begin";
+function App() {
+  return (
+    <>
+      <Begin />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Begin.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Begin = () => {
+  return (
+    <>
+      <div className="dude">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          similique ab quia placeat in blanditiis, ipsum aliquid, maiores magnam
+          saepe quod repudiandae voluptas alias facere? Nulla placeat porro
+          quibusdam aliquam.
+        </p>
+        <a href="#"> Welcome to sass tutorial</a>
+        <ul>
+          <li className="cool">Home</li>
+          <li>Aboutus</li>
+          <li>
+            <a href="https://youtu.be/6Rg3n14PM50">Cool</a>
+          </li>
+        </ul>
+        <h1>Myself Bibekananda Singh</h1>
+        <h2>I'm a Software Engineer</h2>
+      </div>
+    </>
+  );
+};
+
+export default Begin;
